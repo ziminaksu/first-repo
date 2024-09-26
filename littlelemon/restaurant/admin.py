9fe2953a63c505c92c27b764abdd309e933f31c9
Kseniia Zimina <zimina.univer@gmail.com>
2024-09-26 16:21:58 +0200

    initial commit

---
+++ littlelemon/restaurant/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+
+# Register your models here.
+from .models import Menu
+from .models import Booking
+
+admin.site.register(Menu)
